0a7da3e0c11dee066af26367d382228774429cf5
Michael Xavier <michael@michaelxavier.net>
2015-01-12 22:47:32 -0800

    Compiler actually working

--- example/src/Build.purs
+++ example/src/Build.purs
@@ -14,11 +14,34 @@ import Polymer.Types
 import qualified Example.Components.Counter as Counter
 -------------------------------------------------------------------------------
 
-foreign import exit "var exit = require('process').exit;" :: forall e. Fn1 Number (Eff e Unit)
+foreign import exit "var exit = process.exit;" :: forall e. Fn1 Number (Eff e Unit)
+
+foreign import mkdirp """
+  var mdp = require('mkdirp').sync;
+  function mkdirp(p) {
+    return function() {
+      mdp(p);
+      return {};
+    }
+  }
+""" :: forall e. Fn1 String (Eff e Unit)
+
+foreign import writeFileSync """
+  var wfs = require('fs').writeFileSync;
+  function writeFileSync(p, s) {
+    return function() {
+      wfs(p, s)
+      return {};
+    }
+  }
+""" :: forall e. Fn2 String String (Eff e Unit)
 
 
-main = either bail (trace <<< render) mkEl
+main = either bail (write <<< render) mkEl
   where
+    write s = do
+      runFn1 mkdirp "components"
+      runFn2 writeFileSync "components/x-counter.html" s
     bail s = do
       trace s
       runFn1 exit 1
@@ -27,7 +50,7 @@ main = either bail (trace <<< render) mkEl
 -------------------------------------------------------------------------------
 mkEl :: forall e. Either String (PolymerElement Counter.CounterState (timer :: Timer))
 mkEl = do
-  n <- showErr $ mkElementName "counter" -- will explode
+  n <- showErr $ mkElementName "x-counter" -- will explode
   return $ PolymerElement { name: n
                           , attributes: []
                           , markup: Counter.markup
